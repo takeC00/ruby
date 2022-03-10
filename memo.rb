335a85d8ea4df601e01561959f6ce662a4b9d115
服部剛士 <take@TAKE-MacBook.local>
2022-03-11 03:05:37 +0900

    新規作成・追記機能実装

--- memo.rb
+++ memo.rb
@@ -1,25 +1,36 @@
 require "csv"
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
-memo_type = gets.to_s
+memo_type = gets.chomp.to_s
 
 #puts memo_type #入力した文字を確認用
 
-if memo_type === "1\n"
-  puts "メモ新規作成"
-  puts "拡張子を除いたファイル名を入力して下さい"
-  memo_title = gets.to_s
+if memo_type === "1"
+  puts "----メモ新規作成----"
+  puts "ーーーーーーーーーーーーー拡張子を除いたファイル名を入力して下さいーーーーーーーーーーーーー"
+  memo_title = gets.chomp.to_s
   
-  puts "メモ内容を内容を入力してください"
-  memo_text = gets.to_s
+  puts "ーーーーーーーーーーーーーメモ内容を入力してくださいーーーーーーーーーーーーー"
+  memo_text = gets.chomp.to_s
   CSV.open("#{memo_title}.csv",'w') do |csv|
     csv << [memo_text]
   end
   
-elsif memo_type === "2\n"
-  puts "メモ編集"
-  puts "編集したいファイル名を拡張子除いて入力してください"
-  memo_title = gets.to_s
-  p CSV.read("#{memo_title}.csv")
+elsif memo_type === "2"
+  puts "----メモ編集----"
+  Dir.foreach('.') do |item|
+    next if item == '.' or item == '..'
+    puts item
+  end
+  puts "ーーーーーーーーーーーーー上記のcsvファイルから、編集したいファイル名を拡張子を除いて入力してくださいーーーーーーーーーーーーー"
+  memo_title = gets.chomp.to_s
+  
+  CSV.open("#{memo_title}.csv",'a') do |csv|
+    puts "ーーーーーーーーーーーーー#{memo_title}.csvに追記しますーーーーーーーーーーーーー"
+    memo_add = gets.chomp.to_s  
+    csv << [memo_add]
+  end
+ 
+
 else 
   puts "エラー！！正しい番号を入力してください"
 end
